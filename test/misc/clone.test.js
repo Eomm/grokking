42e3742b725e151b1d4527b04e92f9238f97c60b
Manuel Spigolon <behemoth89@gmail.com>
2019-06-22 11:59:36 +0200

    add object create grokking

--- test/misc/clone.test.js
+++ test/misc/clone.test.js
@@ -3,7 +3,7 @@
 const { test } = require('tap')
 
 test('clone object', t => {
-  t.plan(3)
+  t.plan(5)
 
   t.test('spread operator clone only the first depth', t => {
     t.plan(2)
@@ -51,5 +51,43 @@ test('clone object', t => {
 
     aJson.foo.bar = 'doo'
     t.notEqual(cloned.foo.bar, aJson.foo.bar)
+  })
+
+  t.test('object create clone only prototype and properties - function', t => {
+    t.plan(3)
+    function afunc () {
+      this.hello = 'world'
+    }
+
+    afunc.prototype.sayHi = function () {
+      console.log(this.hello)
+    }
+
+    const cloned = Object.create(afunc)
+    t.equals(cloned.hello, afunc.hello)
+
+    afunc.hello = 'ciao'
+    t.equals(cloned.hello, afunc.hello)
+
+    cloned.hello = 'hola'
+    t.notEquals(cloned.hello, afunc.hello)
+  })
+
+  t.test('object create clone only prototype and properties - json', t => {
+    t.plan(3)
+    const aJson = {
+      hello: 'world',
+      foo: {
+        bar: 'bar'
+      }
+    }
+
+    const cloned = Object.create(aJson)
+    t.deepEquals({}, cloned)
+
+    cloned.hello = 'universe'
+    aJson.foo.bar = 'doo'
+    t.equal(cloned.foo.bar, aJson.foo.bar)
+    t.notEqual(cloned.hello, aJson.hello)
   })
 })
